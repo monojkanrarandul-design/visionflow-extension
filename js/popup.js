6223332bf45374a5861cb6da3e907a2ab6b74997
Arijit Paine <arijitpaine647@gmail.com>
2026-07-30 19:00:16 +0530

    added popup.html and logo

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+document.getElementById("openSidePanel").addEventListener("click", async () => {
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+
+    await chrome.sidePanel.open({
+        windowId: tab.windowId,
+    });
+
+    window.close();
+});
